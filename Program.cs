1d07572478feaa0742f767cd62dc9427c22028ea
tqla122-19-4 <ssova228@gmail.com>
2020-11-28 12:58:14 +0200

    ln(lg(e^x)) - 0.35
    ln(lg(e^x)) - 0.35
    calc v1.o

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ooplab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите x:");
+
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Log(Math.Log10(Math.Exp(x))) - 0.35;
+
+            Console.WriteLine("ln(lg(e^x))-0.35=" + y);
+            Console.ReadLine();
+
+        }
+    }
+}
